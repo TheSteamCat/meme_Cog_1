f77b9fafcabce246557c73002d6a691db7063e99
Luci // Seamoss <69442497+TheSteamCat@users.noreply.github.com>
2022-05-06 16:34:45 -0600

    Add files via upload

--- meme_cog_1/megacog.py
+++ meme_cog_1/megacog.py
@@ -1,5 +1,5 @@
 from redbot.core import commands
-
+import random as rand
 
 class MegaCog(commands.Cog):
     """This is a package of my first tests"""
@@ -46,3 +46,31 @@ class MegaCog(commands.Cog):
     async def skillIssue(self, ctx):
         """skill issue"""
         await ctx.send("https://tenor.com/view/doge-difference-in-skill-meme-dog-gif-22631267")
+    
+    @commands.command()
+    async def giraffe(self, ctx):
+        """gives giraffe"""
+        length = 4 #5
+        # sends a random giraffe image
+        x = rand.randint(0, length)
+        # await ctx.send(x) i used this for testing the random system to make sure it works
+        if x == 0:
+            await ctx.send("https://images.ctfassets.net/81iqaqpfd8fy/3r4flvP8Z26WmkMwAEWEco/870554ed7577541c5f3bc04942a47b95/78745131.jpg?w=1200&h=1200&fm=jpg&fit=fill")
+        elif x == 1:
+            await ctx.send("https://api.time.com/wp-content/uploads/2015/09/giraffe.jpg?quality=85&w=800")
+        elif x == 2:
+            await ctx.send("https://worldbirds.com/wp-content/uploads/2020/05/giraffe6.jpg")
+        elif x == 3: 
+            await ctx.send("https://www.publicdomainpictures.net/pictures/270000/nahled/giraffe-1530247467LEm.jpg")
+        elif x == 4:
+            await ctx.send("https://scx2.b-cdn.net/gfx/news/hires/2022/bush-encroaching-sickl.jpg")
+    
+    @commands.command()
+    async def beep(self, ctx):
+        """I am a robot!"""
+        await ctx.send("boop")
+    
+    @commands.command()
+    async def hugprice(self, ctx):
+        """send help"""
+        await ctx.send("https://cdn.discordapp.com/attachments/222078231040229376/970495419094286346/sUaKBaj.jpeg")
